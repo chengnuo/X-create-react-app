a751b344037d589b49a16cf954de7b15bca0355d
xjc <2585441871@qq.com>
2017-06-28 18:05:44 +0800

    报错自动定位并打开编辑器相关位置 webstorm 有错误。到时再研究下这个bug

--- scripts/start.js
+++ scripts/start.js
@@ -3,6 +3,7 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
+process.env.REACT_EDITOR = 'webstorm';  //报错自动定位并打开编辑器相关位置
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
